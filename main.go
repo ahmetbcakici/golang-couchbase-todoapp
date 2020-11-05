476ba96f8990260e89fe6a9621340962dda8ec18
ahmetbcakici <ahmetbcakici@gmail.com>
2020-11-05 22:17:24 +0300

    add cookie config

--- main.go
+++ main.go
@@ -8,15 +8,13 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
+	"time"
 )
 
 type Cat struct {
 	Name string `json:"name"`
 	Age  int32  `json:"age"`
-}
-
-func greetings(c echo.Context) error {
-	return c.String(http.StatusOK, "Welcome to the Echo server!")
 }
 
 func getCats(c echo.Context) error {
@@ -62,30 +60,89 @@ func addCat(c echo.Context) error {
 	return c.String(http.StatusOK, "We got your cat!")
 }
 
+func login(c echo.Context) error {
+	username := c.QueryParam("username")
+	password := c.QueryParam("password")
+
+	// check username and password inside database instead of that
+	if username == "admin" && password == "123" {
+		cookie := &http.Cookie{}
+
+		// this is the same
+		//cookie := new(http.Cookie)
+
+		cookie.Name = "sessionID"
+		cookie.Value = "some_string"
+		cookie.Expires = time.Now().Add(48 * time.Hour)
+
+		c.SetCookie(cookie)
+
+		return c.String(http.StatusOK, "You were logged in!")
+	}
+
+	return c.String(http.StatusUnauthorized, "Your username or password were wrong")
+}
+
+func mainAdmin(c echo.Context) error {
+	return c.String(http.StatusOK, "horay you are on the secret amdin main page!")
+}
+
 func main() {
 	fmt.Println("Hello World!")
 
 	e := echo.New()
+	e.Use(setServerHeader)
 
-	g := e.Group("/admin")
-	g.Use(middleware.Logger())
+	adminGroup := e.Group("/admin")
+	adminGroup.Use(middleware.Logger())
 
-	g.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
-		// check in the DB
-		if username == "ahmet" && password == "123" {
-			return true, nil
-		}
+	/*
+		adminGroup.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
+			// check in the DB
+			if username == "ahmet" && password == "123" {
+				return true, nil
+			}
 
-		return false, nil
-	}))
+			return false, nil
+		}))
 
-	g.GET("/main", func(c echo.Context) error {
-		return c.String(http.StatusOK, "hey")
-	})
+	*/
 
-	e.GET("/", greetings)
+	adminGroup.GET("/main", mainAdmin, checkCookie)
+
+	e.GET("/login", login)
 	e.GET("/cats/:data", getCats)
 	e.POST("/cats", addCat)
 
 	e.Start(":8000")
+}
+
+// MIDDLEWARE
+
+func setServerHeader(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		c.Response().Header().Set(echo.HeaderServer, "ABC-Server/1.0")
+
+		return next(c)
+	}
+}
+
+func checkCookie(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		cookie, err := c.Cookie("sessionID")
+		if err != nil {
+			if strings.Contains(err.Error(), "named cookie not present") {
+				return c.String(http.StatusUnauthorized, "you dont have any cookie")
+			}
+
+			log.Println(err)
+			return err
+		}
+
+		if cookie.Value == "some_string" {
+			return next(c)
+		}
+
+		return c.String(http.StatusUnauthorized, "you dont have the right cookie, cookie")
+	}
 }
